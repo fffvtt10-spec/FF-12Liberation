d7a382d88d3bae84ff4878ab152ada61e72fbab8
Tiago <tgbahiense@gmail.com>
2026-01-25 12:05:04 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Este plugin automatiza a importação do React no JSX
   base: '/FF-12Liberation/', 
 })
