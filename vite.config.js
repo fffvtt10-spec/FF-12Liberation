33dc459303507d65d0a7308d2795e90114e411d6
Tiago <tgbahiense@gmail.com>
2026-01-25 10:47:22 -0300

    injetando deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FF-12Liberation/', // Nome exato do repositório
+  base: '/FF-12Liberation/', 
 })
