d480466023c71fc29ce0de118df42bd51cbd794f
Tiago <tgbahiense@gmail.com>
2026-01-25 12:13:19 -0300

    Fix: Resolvendo conflito de nomes e build do React

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-
-//import React from 'react'
+import React from 'react'; // Adicione no topo do main.jsx
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()], // Este plugin automatiza a importação do React no JSX
+  plugins: [react()], // Este plugin é OBRIGATÓRIO para o React funcionar no build
   base: '/FF-12Liberation/', 
 })
