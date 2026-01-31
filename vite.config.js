e366715a204ee70d3c3d093e6436cafdc4314f92
Tiago <tgbahiense@gmail.com>
2026-01-31 10:44:27 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/", 
   plugins: [react()],
-  base: '/FF-12Liberation/', // O nome do seu repositório deve estar entre barras
 })
